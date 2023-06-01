ac28e37f8af9dc72af04a6ce8a544ebd26a56415
PC <Kamito062004@gmail.com>
2023-06-01 08:05:52 +0700

    update

---
+++ Calculator.java
@@ -0,0 +1,102 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class Calculator {
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+    public static int calculate(int firstOperand, int  secondOperand, char  operator) {
+        switch ( operator) {
+            case ADDITION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Testing addition")
+    void testCalculateAdd() {
+        int firstOperand = 1;
+        int secondOperand = 1;
+        char operator = '+';
+        int expected = 2;
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Testing subtraction")
+    void testCalculateSub() {
+        int firstOperand = 2;
+        int secondOperand = 1;
+        char operator = '-';
+        int expected = 1;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Testing multiplication")
+    void testCalculateMul() {
+        int firstOperand = 2;
+        int secondOperand = 2;
+        char operator = '*';
+        int expected = 4;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Testing division")
+    void testCalculateDiv() {
+        int firstOperand = 6;
+        int secondOperand = 3;
+        char operator = '/';
+        int expected = 2;
+
+        int result = Calculator.calculate(firstOperand, secondOperand, operator);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Testing division by zero")
+    void testCalculateDivByZero() {
+        int firstOperand = 2;
+        int secondOperand = 0;
+        char operator = '/';
+
+        assertThrows(RuntimeException.class,
+                ()-> {Calculator.calculate(firstOperand, secondOperand, operator);});
+    }
+
+    @Test
+    @DisplayName("Testing wrong operator")
+    void testCalculateWrongOperator() {
+        int firstOperand = 2;
+        int secondOperand = 0;
+        char operator = '=';
+
+        assertThrows(RuntimeException.class,
+                ()-> {Calculator.calculate(firstOperand, secondOperand, operator);});
+    }
+}
